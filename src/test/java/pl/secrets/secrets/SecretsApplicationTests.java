5b3b18985e72c3196e260d3dc1641325ec759337
Marek Lewandowski <marek.levandovski@gmail.com>
2019-10-14 22:53:36 +0200

    Add RestAssured

--- src/test/java/pl/secrets/secrets/SecretsApplicationTests.java
+++ src/test/java/pl/secrets/secrets/SecretsApplicationTests.java
@@ -6,6 +6,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static io.restassured.RestAssured.get;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Slf4j
@@ -16,4 +18,8 @@ public class SecretsApplicationTests {
 		log.info("");
 	}
 
+	@Test
+	public void getSecretTest() {
+		get("/secrets").then().statusCode(404);
+	}
 }
